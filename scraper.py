f8aabafa00ba013a857eda0d8aadd667136ae6bd
arafnokib <70985187+arafnokib@users.noreply.github.com>
2021-11-24 12:53:47 -0500

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+browser = webdriver.Chrome("/Users/arafnokib/Downloads/Whitehat/Python/C127HW/chromedriver")
+browser.get(START_URL)
+time.sleep(10)
+def scrape():
+    headers = ["name", "distance", "mass", "radius"]
+    star_data = []
+    for i in range(0, 97):
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        for tr_tag in soup.find_all("tr", attrs={"class", "wikitable sortable jquery-tablesorter"}):
+            td_tags = tr_tag.find_all("td")
+            temp_list = []
+            for index, td_tag in enumerate(td_tags):
+                if index == 0:
+                    temp_list.append(td_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(td_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            star_data.append(temp_list)
+        
+    with open("scraper_1.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(star_data)
+    print(star_data)
+scrape()
